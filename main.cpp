f726bbfa0d010e13ecd3d432ba7c8a50ea52d64d
amandesai01 <amandesai01@gmail.com>
2019-10-25 13:12:48 +0530

    Configured a Repository

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+typedef long double LD;
+typedef long long ll;
+
+#define MOD 1000000007
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define ABS(x) ((x)<0?-(x):(x))
+
+int main()
+{
+    int TESTCASES = 1;
+    scanf("%d", &TESTCASES);
+    while(TESTCASES--){
+        //Your Code Goes Here
+    }
+    return 0;
+}
